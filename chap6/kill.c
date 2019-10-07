84c601bfc72986a801f1e020ff2b205d3241c26f
EL FAIZ MOHAMED <melfaiz@enseirb-matmeca.fr>
2019-10-07 11:44:13 +0200

    kill

---
+++ chap6/kill.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    if(argc<2)
+    {
+        printf("USE : %s PID \n", argv[0]);
+    }else
+    {
+        kill(atoi(argv[1]), SIGKILL);
+        printf("KILLED %i \n", atoi(argv[1]));
+    }
+    
+    
+
+    return 0;
+}
--- chap6/signal.c
+++ chap6/signal.c
@@ -1,17 +1,17 @@
 #include <stdio.h> 
 #include <stdlib.h>
 #include <signal.h> 
+#include <sys/types.h>
+#include <unistd.h>
   
-void handle_sigint(int sig) 
-{ 
-    printf("Caught signal %d\n", sig); 
-} 
-  
-int main() 
-{ 
-    signal(SIGINT, handle_sigint); 
+int main(int argc, char const *argv[])
+{
+
+
+    printf("Le PID du programme %s est : %i \n", argv[0] , getpid() ) ;
 
     while (1);
+  
     
     return 0; 
 } 
